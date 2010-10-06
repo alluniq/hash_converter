4fac6ffba83d351e47cae7954a8bbb132f2582b3
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2010-10-06 10:57:47 +0200

    Checking if mapped key exists, conditional mapping

--- lib/hash_mapper.rb
+++ lib/hash_mapper.rb
@@ -21,8 +21,19 @@ class HashMapper
       @@path.pop
     end
 
-    def self.map(input, output)
+    def self.map(input, output, &block)
       key = @@path + [input]
-      @parsed[output.to_s] = @hash[key.join(SEPARATOR).to_s]
+      key = key.join(SEPARATOR).to_s
+
+      if @hash.has_key?(key)
+        hash_value = @hash[key]
+
+        condition = true
+        if block_given?
+          condition = yield hash_value
+        end
+
+        @parsed[output.to_s] = hash_value if condition
+      end
     end
 end
--- spec/lib/hash_mapper_spec.rb
+++ spec/lib/hash_mapper_spec.rb
@@ -2,6 +2,37 @@ require "spec_helper"
 
 describe HashMapper do
   describe "DSL" do
+    describe "maping" do
+      it "should map from flat hash" do
+        h = {
+          :key1 => "val1",
+          :key2 => "val2",
+          :key3 => "val3"
+        }
+
+        p = HashMapper.parse(h) do
+          map "key1", "a"
+          map "key2", "b"
+          map "key3", "c"
+        end
+
+        p.should == { "a" => "val1", "b" => "val2", "c" => "val3" }
+      end
+
+      it "should not map keys that not exists" do
+        h = {
+          :key  => "value"
+        }
+
+        HashMapper.parse(h) {
+          map "key", "foo"
+          map "foobar", "foobar"
+        }.should == {
+          "foo" => "value"
+        }
+      end
+    end
+
     describe "nesting paths" do
       before do
         @hash = {
@@ -48,6 +79,44 @@ describe HashMapper do
         end
 
         p.should == { "barfoo" => "foobar", "testfoo" => "text" }
+      end
+    end
+
+    describe "conditional mapping" do
+      it "should map argument" do
+        h = {
+          :test => {
+            :valid => 123
+          }
+        }
+
+        p = HashMapper.parse(h) do
+          path "test" do
+            map "valid", "foobar" do |value|
+              value < 200
+            end
+          end
+        end
+
+        p.should == { "foobar" => 123 }
+      end
+
+      it "should not map argument" do
+        h = {
+          :test => {
+            :valid => 123
+          }
+        }
+
+        p = HashMapper.parse(h) do
+          path "test" do
+            map "valid", "foobar" do |value|
+              value > 200
+            end
+          end
+        end
+
+        p.should == {}
       end
     end
   end

0a11d67b80faaa7701dcf0d55c0dd1f69f3dff43
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2010-10-06 09:52:32 +0200

    First parsing functionality.

--- lib/hash_mapper.rb
+++ lib/hash_mapper.rb
@@ -1,2 +1,28 @@
-module HashMapper
+require "core_ext/hash"
+require "pp"
+class HashMapper
+  SEPARATOR = "."
+
+  def self.parse(hash, &block)
+    @hash = hash.namespace_flatten(SEPARATOR)
+    @parsed = {}
+    @@path = []
+
+    instance_eval(&block)
+
+    @parsed
+  end
+
+  private
+
+    def self.path(path, &block)
+      @@path.push(path)
+      instance_eval(&block)
+      @@path.pop
+    end
+
+    def self.map(input, output)
+      key = @@path + [input]
+      @parsed[output] = @hash[key.join(SEPARATOR)]
+    end
 end
--- spec/lib/hash_mapper_spec.rb
+++ spec/lib/hash_mapper_spec.rb
@@ -1,6 +1,33 @@
 require "spec_helper"
 
 describe HashMapper do
-  it "should be" do
+  describe "DSL" do
+    it "should allow to nest paths" do
+      h = {
+        :foo => {
+          :bar => {
+            :text => "foobar"
+          },
+          :test => {
+            :text => "text"
+          }
+        }
+      }
+
+      p = HashMapper.parse(h) do
+        path "foo" do
+          path "bar" do
+            map :text, :barfoo
+          end
+        end
+        path "foo" do
+          path "test" do
+            map :text, :testfoo
+          end
+        end
+      end
+
+      p.should == { :barfoo => "foobar", :testfoo => "text" }
+    end
   end
 end

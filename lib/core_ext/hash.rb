f14a24b97c4fcd3d44d2ad0e3f69aa44415c9c0d
Łukasz Strzałkowski <lukasz.strzalkowski@gmail.com>
2010-10-07 11:40:10 +0200

    Better var names

--- lib/core_ext/hash.rb
+++ lib/core_ext/hash.rb
@@ -1,18 +1,19 @@
 class Hash
   @@namespace = []
+
   def namespace_flatten(separator = ".")
-    h = {}
+    hash = {}
     self.each do |key, value|
-      k = key.to_s
+      key = key.to_s
       if value.is_a?(Hash)
-        @@namespace << k
-        h.merge! value.namespace_flatten(separator)
+        @@namespace << key
+        hash.merge! value.namespace_flatten(separator)
         @@namespace.pop
       else
-        key = @@namespace + [k]
-        h[key.join(separator)] = value
+        key = @@namespace + [key]
+        hash[key.join(separator)] = value
       end
     end
-    h
+    hash
   end
 end
